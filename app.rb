cd206825ddd10230c22bf32beb2d4c6181473e9c
Masaki Komagata <komagata@gmail.com>
2013-01-31 20:46:42 +0900

    Move login function to get '/'

--- app.rb
+++ app.rb
@@ -9,34 +9,34 @@ class PageView
 end
 
 configure do
-  Garb::Session.login(ENV['GOOGLE_ACCOUNT'], ENV['GOOGLE_PASSWORD'])
-  profile = Garb::Management::Profile.all.detect { |p| p.web_property_id == 'UA-2927688-13' }
-  set :profile, profile
 end
 
 get '/' do
+  Garb::Session.login(ENV['GOOGLE_ACCOUNT'], ENV['GOOGLE_PASSWORD'])
+  profile = Garb::Management::Profile.all.detect { |p| p.web_property_id == 'UA-2927688-13' }
+
   yesterday = Time.now.yesterday
-  analytics = settings.profile.page_view(
+  analytics = profile.page_view(
     start_date: yesterday.beginning_of_day,
-    end_date:   yesterday.end_of_day,
+    end_date:   yesterday.end_of_day
   )
   yesterday_pageviews = analytics.first.pageviews
 
-  analytics = settings.profile.page_view(
+  analytics = profile.page_view(
     start_date: yesterday.prev_week,
-    end_date:   yesterday,
+    end_date:   yesterday
   )
   last_week_pageviews = analytics.first.pageviews
 
-  analytics = settings.profile.page_view(
+  analytics = profile.page_view(
     start_date: yesterday.prev_month,
-    end_date:   yesterday,
+    end_date:   yesterday
   )
   last_month_pageviews = analytics.first.pageviews
 
   json pageviews: {
-    yesterday: yesterday_pageviews,
-    last_week: last_week_pageviews,
+    yesterday:  yesterday_pageviews,
+    last_week:  last_week_pageviews,
     last_month: last_month_pageviews
   }
 end
