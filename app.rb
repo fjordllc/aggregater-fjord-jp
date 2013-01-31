7bcf70b8fcbcc2edb97c689fae338fbebbf1400d
Masaki Komagata <komagata@gmail.com>
2013-01-31 19:55:36 +0900

    Add last a week and last a month

--- app.rb
+++ app.rb
@@ -5,20 +5,38 @@ require 'sinatra/json'
 
 class PageView
   extend Garb::Model
-
   metrics :pageviews
 end
 
-get '/yesterday-pageviews' do
+configure do
   Garb::Session.login(ENV['GOOGLE_ACCOUNT'], ENV['GOOGLE_PASSWORD'])
+  profile = Garb::Management::Profile.all.detect { |p| p.web_property_id == 'UA-2927688-13' }
+  set :profile, profile
+end
 
-  profile = Garb::Management::Profile.all.detect { |p| p.web_property_id == ENV['WEB_PROPERTY_ID'] }
-
-  now = Time.now
-  analytics = profile.page_view(
-    start_date: now.yesterday.beginning_of_day,
-    end_date:   now.yesterday.end_of_day,
+get '/yesterday-pageviews' do
+  yesterday = Time.now.yesterday
+  analytics = settings.profile.page_view(
+    start_date: yesterday.beginning_of_day,
+    end_date:   yesterday.end_of_day,
   )
+  json :pageviews => analytics.first.pageviews
+end
 
+get '/last-a-week-pageviews' do
+  yesterday = Time.now.yesterday
+  analytics = settings.profile.page_view(
+    start_date: yesterday.prev_week,
+    end_date:   yesterday,
+  )
+  json :pageviews => analytics.first.pageviews
+end
+
+get '/last-a-month-pageviews' do
+  yesterday = Time.now.yesterday
+  analytics = settings.profile.page_view(
+    start_date: yesterday.prev_month,
+    end_date:   yesterday,
+  )
   json :pageviews => analytics.first.pageviews
 end
